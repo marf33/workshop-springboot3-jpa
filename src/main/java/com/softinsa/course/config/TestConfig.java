02eed0f4a55794d512cec06a8a6fc674df48e236
Miguel Figueiredo <miguel.figueiredo96@gmail.com>
2024-03-05 15:47:19 +0000

    OrderItem, many-to-many association with extra attributes - PART 2

--- src/main/java/com/softinsa/course/config/TestConfig.java
+++ src/main/java/com/softinsa/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.softinsa.course.entities.Category;
 import com.softinsa.course.entities.Order;
+import com.softinsa.course.entities.OrderItem;
 import com.softinsa.course.entities.Product;
 import com.softinsa.course.entities.User;
 import com.softinsa.course.entities.enums.OrderStatus;
 import com.softinsa.course.repositories.CategoryRepository;
+import com.softinsa.course.repositories.OrderItemRepository;
 import com.softinsa.course.repositories.OrderRepository;
 import com.softinsa.course.repositories.ProductRepository;
 import com.softinsa.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository; 
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -69,5 +74,13 @@ public class TestConfig implements CommandLineRunner{
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
 	}
 }
--- src/main/java/com/softinsa/course/entities/OrderItem.java
+++ src/main/java/com/softinsa/course/entities/OrderItem.java
@@ -3,6 +3,7 @@ package com.softinsa.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.softinsa.course.entities.pk.OrderItemPK;
 
 import jakarta.persistence.EmbeddedId;
@@ -17,7 +18,7 @@ public class OrderItem implements Serializable{
 	
 	//Primary key id composto
 	@EmbeddedId
-	private OrderItemPK id;
+	private OrderItemPK id = new OrderItemPK();
 	
 	private Integer quantity;
 	private Double price;
@@ -34,6 +35,7 @@ public class OrderItem implements Serializable{
 		this.price = price;
 	}
 	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
