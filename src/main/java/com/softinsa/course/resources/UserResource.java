8e9121d5a5df394f05ebeaf1df7373be450473c1
Miguel Figueiredo <miguel.figueiredo96@gmail.com>
2024-03-05 09:57:29 +0000

    Service layer, component registration

--- src/main/java/com/softinsa/course/resources/UserResource.java
+++ src/main/java/com/softinsa/course/resources/UserResource.java
@@ -1,21 +1,39 @@
 package com.softinsa.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.softinsa.course.entities.User;
+import com.softinsa.course.services.UserService;
+
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
 	//RespondeEntity retorna resposta de requisições web
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "999999999", "12345");
+	public ResponseEntity<List<User>> findAll(){
+
+		List<User> list = service.findAll();
 		//Retornar resposta OK no http
-		return ResponseEntity.ok().body(u);
+		return ResponseEntity.ok().body(list);
 	}
+	
+	//Buscar user por Id
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
